15ac76e5db90f788aa8256ab14a5bd8028312b8b
Philip Fung <philip.w.fung@gmail.com>
2017-04-11 12:15:46 -0700

    dudnsbutton: added example to header

--- DudNSButton.swift
+++ DudNSButton.swift
@@ -1,8 +1,14 @@
 //
 //  DudNSButton.swift
-//  
 //
 //  Created by Philip Fung on 4/11/17.
+//
+//  Usage:
+//  
+//  let buttonView = DudNSButton.init(frame: NSRect(x: 0, y: 0, width: 300.0, height: 150.0))
+//  buttonView.setBackgroundColor(color: NSColor.blue)
+//  buttonView.setBorderAttributes(color: NSColor.black, borderWidth: 2.0, cornerRadius: 5.0)
+//  buttonView.setText(text: "Submit", font: NSFont.boldSystemFont(ofSize: 16.0), color: NSColor.white)
 //
 //  MIT License
 
